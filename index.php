c024380882cda32460f08580e92c433b770145e6
Man Chu <rmc456@email.nyu.edu>
2014-01-31 19:41:37 -0500

    added Jan30 self portrait assignment

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
 </head>
 <body>
 
-	Hello World!
+	<h3>Game Development Studio I</h3>
+	<h4>Man Chu (Raymond)</h4>
+
+	<br><br>
+
+	<a href="projects/01-30.html">self portrait</a><br>
+	<a>[Homework] Landscape</a>
 
 </body>
 </html>
